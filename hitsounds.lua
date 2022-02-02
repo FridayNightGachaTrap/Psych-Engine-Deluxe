15ba10da444bb6bd6cb57478b2b63814a181f392
jmxamongusmodder <80003646+jmxamongusmodder@users.noreply.github.com>
2022-02-02 17:59:19 -0500

    this is so old bro

--- hitsounds.lua
+++ hitsounds.lua
@@ -2,7 +2,7 @@ function onUpdate()
     if getPropertyFromClass('flixel.FlxG', 'keys.justPressed.Q') then
         playSound('cancelMenu', 1)
         function goodNoteHit(id, direction, noteType, isSustainNote)
-            playSound('ChartingTick', 0.3)
+            playSound('ChartingTick', 0.3) -- BRO WHAT THIS IS SO OLD THIS AINT EVEN USED ANYMORE
         end
     end
 end
